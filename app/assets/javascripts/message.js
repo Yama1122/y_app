4ded8f6fa51055512599a7ba21276553c491d7d3
Yama1122 <yamashita1122.you@gmail.com>
2020-07-16 23:45:31 +0900

    IndexView_2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,5 +2,27 @@ $(function(){
   $(".sidebar__user__icon--bars").on("click", function(){
     $(".sidebar__config").slideToggle();
     $(".sidebar__config").css("display","flex");
-  })
+  });
+
+  $(".mainchat__info__left__membercount").on("click", function(){
+    $(".mainchat__members").slideToggle().css({
+      display:"flex"
+    });
+
+
+    $(".form").css({
+      border:"none",
+      padding:"410px 0 0 0"
+    });
+
+    $(".mainchat__info").css({
+      border:"solid 1px black"
+    });
+
+    $(".mainchat__messagelist").css({
+      height:"calc(100vw - 105px)"
+    });
+
+    
+  });
 });
